5853688f2ebf95a70b307ef5b7ba1e7275fe949c
Your Name <you@example.com>
2020-09-14 22:51:36 +0800

    [luyuehuan] feat:should_return_total_score_when_the_tenth_is_strike

--- src/main/java/BowlinGame.java
+++ src/main/java/BowlinGame.java
@@ -1,9 +1,15 @@
 public class BowlinGame {
     public int calculateScore(int[] score) {
         int totalScore = 0;
-        for (int i = 0; i < 10; ++i) {
+        for (int i = 0; i < score.length - 2; ++i) {
             if (score[i] == 10) {
                 totalScore += score[i] + score[i + 1] + score[i + 2];
+            }
+            else if ((score[i] + score[i + 1]) == 10) {
+                totalScore += 10 + score[i + 2];
+                i++;
+            } else {
+                totalScore += score[i];
             }
         }
         return totalScore;
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -18,4 +18,12 @@ public class BowlingGameTest {
         int totalScore = bowlinGame.calculateScore(score);
         assertEquals(300, totalScore);
     }
+
+    @Test
+    void should_return_total_score_when_the_tenth_is_strike() {
+        BowlinGame bowlinGame = new BowlinGame();
+        int[] score = {1, 1, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 1, 1};
+        int totalScore = bowlinGame.calculateScore(score);
+        assertEquals(40, totalScore);
+    }
 }
